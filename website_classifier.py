1412f348e6f24b34259bd6e78ead79138f0346b3
sandydesu@gmail.com <sandydesu@gmail.com>
2020-04-07 15:07:43 +0100

    website_classifier.py threading complete

--- website_classifier.py
+++ website_classifier.py
@@ -2,45 +2,56 @@ import pandas as pd
 import requests
 import os
 from google.cloud import language
+from concurrent.futures import as_completed, ThreadPoolExecutor
 
 
+N_THREADS = 100
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/sandylee/.secret/backup-142521-18d7e8c2e792.json"
 
 
-def get_html(url):
-    return requests.get(url)
 
-
-def classify(text, verbose=True):
+def classify(url):
 
     """Classify input text into categories. """
 
+    text = requests.get(url).content
+
     language_client = language.LanguageServiceClient()
-    document = language.types.Document(
-        content=text,
-        type=language.enums.Document.Type.HTML)
+    document = language.types.Document(content=text,
+                                       type=language.enums.Document.Type.HTML)
     response = language_client.classify_text(document)
     categories = response.categories
     result = {}
     for category in categories:
         result[category.name] = category.confidence
 
+    # data = list(result.items())
+    # print(url)
+    # print('=' * len(url))
+    # print(f'Primary Category: {data[0][0].split("/")[1]}')
+    # print(f'Secondary Category: {data[0][0].split("/")[2]}')
+    # print(f'Confidence: {round(data[0][1] * 100)}%')
+
     return result
 
 
 def main():
-
     serp_results = pd.read_csv("serp_results.csv")
-    urls = serp_results.result[10:15]
-    for url in urls:
-        webpage = get_html(url)
-        classification = classify(webpage.text)
-        data = list(classification.items())
-        print(url)
-        print('='* len(url))
-        print(f'Primary Category: {data[0][0].split("/")[1]}')
-        print(f'Secondary Category: {data[0][0].split("/")[2]}')
-        print(f'Confidence: {round(data[0][1] * 100)}%')
+    urls = serp_results.result
+
+    pool = ThreadPoolExecutor(max_workers=N_THREADS)
+
+    futures = {pool.submit(classify, url): url for url in urls[:5]}
+    for future in as_completed(futures):
+
+        print(future.result())
+
+
+
+    # for url in urls:
+    #     webpage = get_html(url)
+    #     classification = classify(url, webpage.text)
+
 
 
 
